6393fdc3e23441a0243e6ffaca5d3926a54723ef
Jakub Smid <jakubsmid22@gmail.com>
2024-07-16 17:07:10 +0200

    Completed html and css

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const timerSwitch = document.getElementById("timer");
 const stopwatchSwitch = document.getElementById("stopwatch");
 const switchs = document.querySelectorAll("[data-switch]");
+const timerContainer = document.getElementById("timerContainer");
+const stopwatchContainer = document.getElementById("stopwatchContainer");
 
 switchs.forEach((e, i) => {
     e.addEventListener("click", () => {
         switchs.forEach(x => x.classList.remove("active"));
-        e.id === "timer" ? timerSwitch.classList.add("active") : stopwatchSwitch.classList.add("active");
+        if (e.id === "timer") {
+            timerSwitch.classList.add("active");
+            timerContainer.classList.remove("-left-60");
+            timerContainer.classList.add("left-0");
+            timerContainer.classList.add("w-full");
+            stopwatchContainer.classList.remove("left-0");
+            stopwatchContainer.classList.remove("w-full");
+            stopwatchContainer.classList.add("-left-60");
+        }
+        else {
+            stopwatchContainer.classList.remove("-left-60");
+            timerContainer.classList.remove("left-0");
+            stopwatchContainer.classList.add("w-full");
+            timerContainer.classList.remove("w-full");
+            stopwatchSwitch.classList.add("active")
+            timerContainer.classList.add("-left-60");
+            stopwatchContainer.classList.add("left-0");
+        }
     });
 })
